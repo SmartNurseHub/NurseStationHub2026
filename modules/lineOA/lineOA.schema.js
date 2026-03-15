fce94cc49b77915666f09a598f18199c88cbc893
SmartNurseHub <pakmud.nursing@gmail.com>
2026-03-16 00:22:42 +0700

    modified2026-03-16

--- modules/lineOA/lineOA.schema.js
+++ modules/lineOA/lineOA.schema.js
@@ -3,16 +3,21 @@ module.exports = {
   /* ===============================
      LINE DATA
   =============================== */
+
   FOLLOW_SHEET: "FollowList",
   USER_SHEET: "UserList",
+
+  // ⭐ เพิ่มบรรทัดนี้
+  LINE_UID_SHEET: "LineUID",
 
   /* ===============================
      NURSING RECORDS (ส่งผลตรวจ)
   =============================== */
+
   NURSING_SHEET: "NursingRecords",
 
   NURSING_COLUMNS: {
-    NSR: 0,          // เลขใบงาน
+    NSR: 0,
     IDCARD: 1,
     STATUS: 2,
     USERID: 3,
@@ -22,7 +27,7 @@ module.exports = {
     RESULT: 7,
     ADVICE: 8,
     FILEURL: 9,
-    SENTSTAMP: 10    // เวลาที่ส่งแล้ว
+    SENTSTAMP: 10
   }
 
 };
